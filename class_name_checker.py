03463d75f6b1da8cc99149519645b2fc919b4714
shrav20 <91880988+shrav20@users.noreply.github.com>
2023-12-08 16:50:12 +0530

    Create class_name_checker.py
    used to check if the class name starts with verb

---
+++ class_name_checker.py
@@ -0,0 +1,25 @@
+from pylint.interfaces import IAstroidChecker
+from pylint.checkers import BaseChecker
+from pylint.checkers.utils import check_messages
+
+class ClassNameChecker(BaseChecker):
+    __implements__ = IAstroidChecker
+
+    name = 'class-name-checker'
+    priority = -1
+    msgs = {
+        'C9999': (
+            'Class name should start with a verb',
+            'class-name-should-start-with-verb',
+            'Class names should start with a verb.'
+        ),
+    }
+
+    def visit_classdef(self, node):
+        class_name = node.name.lower()
+        verbs = ['create', 'initialize', 'generate', 'compute']  # Add more verbs as needed
+        if not any(class_name.startswith(verb) for verb in verbs):
+            check_messages(self, 'class-name-should-start-with-verb', node=node)
+
+def register(linter):
+    linter.register_checker(ClassNameChecker(linter))
