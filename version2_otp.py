39c4bad9c14a775eadc4ff69d49671473e9ea61d
shrav20 <shravanivv.chavan@gmail.com>
2024-05-08 19:31:45 +0530

    version2 updated

--- version2_otp.py
+++ version2_otp.py
@@ -1,13 +1,12 @@
-# Import your functions here
-import otp_swanand_2130331245033 as otp
 import unittest
 import pytest
+import otp_swanand_2130331245033 as otp
 
 class ComputeOTPFunctions(unittest.TestCase):
 
     def test_generate_otp(self):
-        """Test the generateOtp function."""
-        result = otp.generate_otp(8)
+        """Test the generate_otp function."""
+        result = otp.CreateCommunicatingService.generate_otp(8)
         expected_len = 8
 
         result_isint = result.isdigit()
@@ -15,22 +14,24 @@ class ComputeOTPFunctions(unittest.TestCase):
 
         self.assertEqual(len(result), expected_len)
         self.assertEqual(result_isint, expected_isint)
+
     def test_validate_emailid(self):
         """Test validate Email."""
-        result = otp.validate_email("test@gmail.com")
+        result = otp.EmailService.validate_email("test@gmail.com")
         expected = True
 
         self.assertEqual(result, expected)
 
-        result = otp.validate_email("test@dbatu.ac.in")
-
+        result = otp.EmailService.validate_email("test@dbatu.ac.in")
         self.assertEqual(result, expected)
+
     def test_validate_mobile(self):
         """Test validate Mobile number."""
         mobile = "8625847883"
-        result = otp.validate_mobile(mobile)
+        result = otp.MobileService.validate_mobile(mobile)
         expected = True
 
         self.assertEqual(result, expected)
+
 if __name__ == "__main__":
     pytest.main()
