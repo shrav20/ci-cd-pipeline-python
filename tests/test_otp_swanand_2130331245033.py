5a397261abc50f65204b6d18da311cbb97a50d11
shrav20 <91880988+shrav20@users.noreply.github.com>
2024-06-13 18:56:52 +0530

    Update test_otp_swanand_2130331245033.py

--- tests/test_otp_swanand_2130331245033.py
+++ tests/test_otp_swanand_2130331245033.py
@@ -1,6 +1,6 @@
+import os
 import random
 import re
-#import os
 import smtplib
 # pylint: disable=import-error
 from twilio.rest import Client
@@ -84,8 +84,8 @@ if __name__ == "__main__":  # Corrected "__main__" instead of "_main_"
     account_sid_value = 'AC1a01a4fd1cc7cdbb358e19fe12b9ce93'  # pylint: disable=C0103
     auth_token_value = '1fbcb17dfe649c3d4476b8d0330e07dc'  # pylint: disable=C0103
     twilio_number = '+15735944610'  # pylint: disable=C0103
-    sender_email = "swanandbhuskute567@gmail.com"  # pylint: disable=C0103
-    sender_password = "gvkguusgyahnhnfe"  # pylint: disable=C0103
+    sender_email = os.getenv('SENDER_EMAIL')  # pylint: disable=C0103
+    sender_password = os.getenv('SENDER_PASSWORD')  # pylint: disable=C0103
     # pylint: enable=W0621
     otp_services = GenerateOTPServices(account_sid_value, auth_token_value, twilio_number, sender_email, sender_password)
 
